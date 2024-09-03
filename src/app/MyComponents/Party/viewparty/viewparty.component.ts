4de878cbf883b7b628290af12371a5bff27f32b0
YashpalGawali <gawaliyashpal@gmail.com>
2024-09-03 17:39:14 +0530

    Party component added

--- src/app/MyComponents/Party/viewparty/viewparty.component.ts
+++ src/app/MyComponents/Party/viewparty/viewparty.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PartyService } from 'src/app/Services/party.service';
 
 @Component({
   selector: 'app-viewparty',
   templateUrl: './viewparty.component.html',
   styleUrls: ['./viewparty.component.css']
 })
-export class ViewpartyComponent {
+export class ViewpartyComponent implements OnInit{
 
+  response : any
+  reserr : any
+  party_list : any
+  
+  constructor(private partyserv : PartyService,private router  : Router){ }
+  
+  ngOnInit(): void {
+    this.partyserv.getAllParties().subscribe({
+      next : (data) => {
+          this.party_list = data
+      },
+    })
+  }
+
+  getPartyById(id : number) {
+    this.router.navigate(['edit/party/',id])
+  }
 }
