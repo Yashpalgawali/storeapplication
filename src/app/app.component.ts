2c0796b6e56076d6d6ca5b7a4ea3b9892a3d9369
YashpalGawali <gawaliyashpal@gmail.com>
2024-12-05 15:21:34 +0530

    05 Dec sec commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { JwtAuthServiceService } from './Services/jwt-auth-service.service';
-import { timeInterval } from 'rxjs';
+import { filter, timeInterval } from 'rxjs';
 import { ViewinvoicetemplateComponent } from './MyComponents/Invoice/viewinvoicetemplate/viewinvoicetemplate.component';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'storeapp';
   isLoggedIn : any
-  
-  constructor(private jwtauthserv : JwtAuthServiceService) { 
+  showFooter !: boolean
+  constructor(private jwtauthserv : JwtAuthServiceService,private router : Router) { 
    
     setInterval(() => {
       this.isLoggedIn = jwtauthserv.getAuthenticatedUser()
       }, 1);
+
   }
 
-   
- 
+  ngOnInit(): void {
+       // Listen for route changes to toggle footer visibility
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(() => {
+      // Check if the current route is '/login' to hide the footer
+      const currentRoute = this.router.url;
+      this.showFooter = currentRoute !== '/login';
+    });
+  }
 }
